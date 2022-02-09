9a7d1ac0e1de626e9b64891c0ea79ba2004ed216
mikiyoshi <mikiyoshi.kokura@gmail.com>
2022-02-09 20:50:04 +0000

    add connected to a PostgreSQL database function

---
+++ bootcamp_app/students.js
@@ -0,0 +1,99 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx',
+});
+
+// pool
+//   .query(
+//     `
+// SELECT id, name, cohort_id
+// FROM students
+// LIMIT 5;
+// `
+//   )
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => console.error('query error', err.stack));
+
+// Result {
+//   command: 'SELECT',
+//   rowCount: 5,
+//   oid: null,
+//   rows: [ ... ],
+//   ...
+
+// pool
+//   .query(
+//     `
+// SELECT id, name, cohort_id
+// FROM students
+// LIMIT 5;
+// `
+//   )
+//   .then((res) => {
+//     res.rows.forEach((user) => {
+//       console.log(
+//         `${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`
+//       );
+//     });
+//   });
+
+//   id |       name       | cohort_id
+//   ----+------------------+-----------
+//     1 | Armand Hilll     |         1
+//     2 | Stephanie Wolff  |         1
+//     3 | Stan Miller      |         1
+//     4 | Elliot Dickinson |         1
+//     5 | Lloyd Boehm      |         1
+
+// pool
+//   .query(
+//     `
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// LIMIT 5;
+// `
+//   )
+//   .then((res) => {
+//     res.rows.forEach((user) => {
+//       console.log(
+//         `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+//       );
+//     });
+//   });
+
+//   vagrant [bootcamp_app]> node students.js
+// Armand Hilll has an id of 1 and was in the FEB12 cohort
+// Stephanie Wolff has an id of 2 and was in the FEB12 cohort
+// Stan Miller has an id of 3 and was in the FEB12 cohort
+// Elliot Dickinson has an id of 4 and was in the FEB12 cohort
+// Lloyd Boehm has an id of 5 and was in the FEB12 cohort
+
+pool
+  .query(
+    `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`
+  )
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+      );
+    });
+  })
+  .catch((err) => console.error('query error', err.stack));
+
+//   vagrant [bootcamp_app]> node students.js FEB 2
+//   Armand Hilll has an id of 1 and was in the FEB12 cohort
+//   Stephanie Wolff has an id of 2 and was in the FEB12 cohort
---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,37 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx',
+});
+
+pool
+  .query(
+    `
+    SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+    FROM teachers
+    JOIN assistance_requests ON teacher_id = teachers.id
+    JOIN students ON student_id = students.id
+    JOIN cohorts ON cohort_id = cohorts.id
+    WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+    ORDER BY teacher;
+`
+  )
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(`${user.cohort} ${user.teacher}`);
+    });
+  })
+  .catch((err) => console.error('query error', err.stack));
+
+// vagrant [bootcamp_app]> node teachers.js
+// JUL02 Cheyanne Powlowski
+// JUL02 Georgiana Fahey
+// JUL02 Helmer Rodriguez
+// JUL02 Jadyn Bosco
+// JUL02 Roberto Towne
+// JUL02 Rosalyn Raynor
+// JUL02 Talon Gottlieb
+// JUL02 Waylon Boehm
